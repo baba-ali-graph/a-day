4ca4fe53b9bdb424931f97388504abdfcd6fa8a4
Baba Ali <babaali196@gmail.com>
2023-02-01 23:10:19 +0100

    improvements on hourly app

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const DAILY_HOURS = 24;
 const INTERVAL = 15;
 const HOURLY_MINUTES = 60;
 const HR_INDICATOR_CLASS = ".hr-indicator";
-let unitsIndicatorEl, remainingEl, passedEl, hoursEls;
+let unitsIndicatorEl, remainingEl, passedEl, hoursEls, minutesLevelEl;
 
 main();
 
@@ -11,6 +11,7 @@ function main() {
   remainingEl = document.querySelector('[data-key="remaining"]');
   passedEl = document.querySelector('[data-key="passed"]');
   hoursEls = document.querySelectorAll(".hr");
+  minutesLevelEl = document.querySelector(".minutes-level");
   onTick();
   window.setInterval(onTick, 5000);
 }
@@ -30,7 +31,11 @@ function onTick() {
 
   const passedInMinutes = currHour * 60 + today.getMinutes();
   const heightOfIndicator = (passedInMinutes / (DAILY_HOURS * 60)) * 100;
+  const widthOfMinutesLevel = ((INTERVAL - minutesModulo) / INTERVAL) * 100;
+
   unitsIndicatorEl.style.height = `${heightOfIndicator}%`;
+  minutesLevelEl.style.width = `${widthOfMinutesLevel}%`;
+
   hoursUpdater(currHour, today.getMinutes());
 }
 
